8c3fe378a0bd5a15004c6d8c59a803486ccd86f6
Trewean <71116135+Trewean@users.noreply.github.com>
2022-06-18 17:08:26 +0300

    Add files via upload

---
+++ MaxArray.php
@@ -0,0 +1,40 @@
+<?php
+
+function FillArray($num){
+    $array = array();
+    for($i = 0; $i < $num; $i++){
+        array_push($array, random_int(0, 100));
+    }
+    return $array;
+}
+
+function FindMaxArray($array){
+    $first = 0;
+    $len = 1;
+    $maxArray = array();
+    for($i = 1; $i < count($array); $i++){
+        if($array[$i] >= $array[$i - 1]) $len++;
+        else{
+            if($len > count($maxArray)) $maxArray = array_slice($array, $first, $len);
+
+            $first = $i;
+            $len = 1;
+        }
+    }
+    if($len > count($maxArray)) $maxArray = array_slice($array, $first, $len);
+    return $maxArray;
+}
+
+$array = FillArray(20);
+//$array = array(4, 1, 5, 11, 15, 1, 4, 6, 7, 8);
+foreach ($array as $item){
+    echo $item." ";
+}
+echo nl2br("\r\n");
+
+$maxArray = FindMaxArray($array);
+
+foreach ($maxArray as $item){
+    echo $item." ";
+}
+echo nl2br("\r\n");
