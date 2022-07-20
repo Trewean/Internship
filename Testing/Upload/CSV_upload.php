e6ef0494f01be0ecd6e1d390bf76d8518a12a6b0
Trewean <71116135+Trewean@users.noreply.github.com>
2022-07-20 16:19:42 +0300

    Add files via upload

---
+++ Testing/Upload/CSV_upload.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<form action="upload.php" enctype="multipart/form-data" method="post">
+    <p>Добавить csv:<input type="file" name="csv_file"></p>
+    <p><input type="submit"></p>
+</form>
+</body>
+</html>
---
+++ Testing/Upload/upload.php
@@ -0,0 +1,22 @@
+<?php
+if($_FILES['csv_file']['type'] != "text/csv"){
+    ?>
+    <p>Неверный тип файла, выберите заново</p>
+    <a href = "CSV_upload.php" ><button>Вернуться к форме</button></a>
+<?php
+}
+else{
+    move_uploaded_file($_FILES['csv_file']['tmp_name'], $_FILES['csv_file']['name']);
+    if(($file = fopen($_FILES['csv_file']['name'], "r")) !== false){
+        if(!file_exists("upload")) mkdir("upload");
+        while(($data = fgetcsv($file,100 , ",")) !== false){
+            file_put_contents("upload/".$data[0], $data[1]);
+            //var_dump($data);
+        }
+    }
+    ?>
+<p>Загрузка завершена</p>
+<a href="CSV_upload.php"><button>Вернуться к форме</button></a>
+<?php
+    unlink($_FILES['csv_file']['name']);
+}
